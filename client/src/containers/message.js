cd49e2c01877af29f9da59cf7e205a95ede9c064
momojunzi <mark.holthaus@gmail.com>
2018-02-19 12:54:10 -0800

    a little change to the video

--- client/src/containers/message.js
+++ client/src/containers/message.js
@@ -16,6 +16,7 @@ class Message extends Component {
     return (
       <div style={vidDivStyle} className="embed-responsive embed-responsive-16by9">
         <video controls style={vidStyle} className="embed-responsive-item" onEnded={this.props.endHandler}>
+          <source src="https://res.cloudinary.com/momojunzi/video/upload/ac_aac,af_48000,c_fit,h_480,vs_30s,w_640/v1518662857/banquet1_dqnmb6.mp4" type="video/mp4"/>
           <source src="http://res.cloudinary.com/momojunzi/video/upload/v1518662881/banquet1_bttayb.webm" type="video/webm"/>
           <source src="http://res.cloudinary.com/momojunzi/video/upload/v1518662857/banquet1_dqnmb6.mp4" type="video/mp4"/>
         </video>
